53ec722f1afff85e4841355113d6ad6bf860490a
wllcosta <wllcosta0302@gmail.com>
2025-11-12 15:50:37 -0300

    adicionar o cors para o render se comunicar com a vercel

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const app = require('./src/app');
+const express = require("express");
+const cors = require("cors");
+const app = require("./src/app");
 
 const port = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:4200",
+      "https://descarta-ai-frontend.vercel.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.listen(port, () => {
-    console.log(`Servidor DescartAI rodando em http://localhost:${port}`);
+  console.log(`Servidor DescartAI rodando em http://localhost:${port}`);
 });
