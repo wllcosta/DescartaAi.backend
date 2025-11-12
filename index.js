12989874ac6b0216fe428c8b94c1833b7da67e6f
wllcosta <wllcosta0302@gmail.com>
2025-11-12 18:25:26 -0300

    ajuste no cors 2

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:4200",
-      "https://descarta-ai-frontend.vercel.app",
-    ],
+    origin: "https://descarta-ai-frontend.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Servidor DescartAI rodando na porta ${port}`);
+app.listen(port, () => {
+  console.log(`Servidor DescartAI rodando em http://localhost:${port}`);
 });
